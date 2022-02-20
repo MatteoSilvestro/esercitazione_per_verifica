feb8828a473069a1c324986de7704ebba0e57ef7
MatteoSilvestro <75260237+MatteoSilvestro@users.noreply.github.com>
2022-02-20 22:29:48 +0100

    Add files via upload

---
+++ Libri.java
@@ -0,0 +1,19 @@
+package esercizio_libri;
+
+public class Libri {
+
+	public String nome;
+	public int numero;
+	public String categoria;
+	public int prezzo; 
+			
+	public Libri(String nome1,int numero1,String categoria1,int prezzo1) {
+		nome = nome1;
+		numero = numero1;
+		categoria = categoria1;
+		prezzo = prezzo1;
+	}
+	
+	
+
+}
